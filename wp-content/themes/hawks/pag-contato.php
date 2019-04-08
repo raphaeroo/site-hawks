170053f5c12db8782da2d35a04cc2e045a72a9fb
Raphael <raphael@hawks.com.br>
2019-04-08 09:31:06 -0300

    pag contato update

---
+++ wp-content/themes/hawks/pag-contato.php
@@ -0,0 +1,20 @@
+<?php
+/* Template Name: pag-contato */
+get_header();
+?>
+<section id="topo-contato"></section>
+<div class="clear" style="margin-top: 20em;"></div>
+<section id="content-contato">
+<div class="container">
+  <div class="level">
+    <div class="level-item">
+      <h2 class="title">Contato</h2><br>
+      <p class="subtitle">Deixe sua mensagem e entraremos em contato o mais breve possível.</p>
+    </div>
+  </div>
+  <div class="row mt-4">
+
+  </div>
+</div>
+</section>
+<?php get_footer();?>
